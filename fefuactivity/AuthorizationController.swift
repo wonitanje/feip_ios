9a21f4c89eaec7d2aa0b8139fb477d4c4143d9eb
wonitanje <prod.juswee@gmail.com>
2021-11-03 14:39:53 +1000

    Add Navigation, Views, Controllers and custom buttons

---
+++ fefuactivity/AuthorizationController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class AuthorizationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func authorize(_ sender: Any) {
+        
+    }
+}
